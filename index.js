3d9b6c4700ed4adf5138b3eca27c646dd835be1b
vijay4211 <vijayppandit2042@gmail.com>
2022-06-16 15:04:41 +0530

    add some onclick funcationality

--- index.js
+++ index.js
@@ -19,3 +19,35 @@ function buyProduct(){
     console.log("hello")
     window.location.href="https://www.amazon.in/Fossil-Touchscreen-Smartwatch-Smartphone-Notifications/dp/B07SRVV8V4"
 }
+
+
+function facebook(){
+    // console.log("hello")
+    window.location.href="https://www.facebook.com/login.php?skip_api_login=1&api_key=2140564542847120&kid_directed_site=0&app_id=2140564542847120&signed_next=1&next=https%3A%2F%2Fwww.facebook.com%2Fv12.0%2Fdialog%2Foauth%3Fapp_id%3D2140564542847120%26cbt%3D1655291576393%26channel_url%3Dhttps%253A%252F%252Fstaticxx.facebook.com%252Fx%252Fconnect%252Fxd_arbiter%252F%253Fversion%253D46%2523cb%253Df2f6440c7e1c47c%2526domain%253Daccounts.hindustantimes.com%2526is_canvas%253Dfalse%2526origin%253Dhttps%25253A%25252F%25252Faccounts.hindustantimes.com%25252Ff7a866e749d8fc%2526relation%253Dopener%26client_id%3D2140564542847120%26display%3Dpopup%26domain%3Daccounts.hindustantimes.com%26e2e%3D%257B%257D%26fallback_redirect_uri%3Dhttps%253A%252F%252Faccounts.hindustantimes.com%252F%253FhtMoe%253D79e15a45-72cb-4da0-a678-de84f863d4a3%26locale%3Den_US%26logger_id%3Df3d5f84938e9d7c%26origin%3D1%26redirect_uri%3Dhttps%253A%252F%252Fstaticxx.facebook.com%252Fx%252Fconnect%252Fxd_arbiter%252F%253Fversion%253D46%2523cb%253Df2d6de859f37b14%2526domain%253Daccounts.hindustantimes.com%2526is_canvas%253Dfalse%2526origin%253Dhttps%25253A%25252F%25252Faccounts.hindustantimes.com%25252Ff7a866e749d8fc%2526relation%253Dopener%2526frame%253Df85301ea5951ac%26response_type%3Dtoken%252Csigned_request%252Cgraph_domain%26return_scopes%3Dtrue%26scope%3Dpublic_profile%252Cemail%26sdk%3Djoey%26version%3Dv12.0%26ret%3Dlogin%26fbapp_pres%3D0%26tp%3Dunspecified&cancel_url=https%3A%2F%2Fstaticxx.facebook.com%2Fx%2Fconnect%2Fxd_arbiter%2F%3Fversion%3D46%23cb%3Df2d6de859f37b14%26domain%3Daccounts.hindustantimes.com%26is_canvas%3Dfalse%26origin%3Dhttps%253A%252F%252Faccounts.hindustantimes.com%252Ff7a866e749d8fc%26relation%3Dopener%26frame%3Df85301ea5951ac%26error%3Daccess_denied%26error_code%3D200%26error_description%3DPermissions%2Berror%26error_reason%3Duser_denied&display=popup&locale=en_GB&pl_dbl=0"
+
+}
+
+function linkdin(){
+    // console.log("hello")
+    window.location.href="https://www.linkedin.com/signup"
+
+
+}
+
+function instagram(){
+    // console.log("hello")
+    window.location.href="https://www.instagram.com/"
+
+
+}
+
+function twitter(){
+    // console.log("hello")
+    window.location.href="https://twitter.com/i/flow/login"
+
+
+}
+
+function signin(){
+    window.location.href="https://accounts.hindustantimes.com/?htMoe=79e15a45-72cb-4da0-a678-de84f863d4a3"
+}
