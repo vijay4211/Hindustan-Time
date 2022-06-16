f754ed4bcf4f1e6f69817d26d151958f2637475e
vijay4211 <vijayppandit2042@gmail.com>
2022-06-16 14:48:11 +0530

    crat footer bar and add some js funcationality

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+function buyProduct(){
+    console.log("hello")
+    window.location.href="https://www.amazon.in/11X-Cosmic-Black-128GB-Storage/dp/B085J1DRPG/ref=asc_df_B085J1DRPG/?tag=googleshopdes-21&linkCode=df0&hvadid=397001268433&hvpos=&hvnetw=g&hvrand=12412044277660151580&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1007780&hvtargid=pla-1359252477253&psc=1&ext_vrnc=hi"
+}
+
+function buyProduct(){
+    console.log("hello")
+    window.location.href="https://www.amazon.in/Universal-Smartphone-Teleprompter-Production-FC-TP-EZ/dp/B06XC4J615"
+}
+
+function buyProduct(){
+    console.log("hello")
+    window.location.href="https://www.amazon.in/PLAYTM-MP1-Smart-WiFi-Projector/dp/B07QZLSGM9"
+}
+
+function buyProduct(){
+    console.log("hello")
+    window.location.href="https://www.amazon.in/Fossil-Touchscreen-Smartwatch-Smartphone-Notifications/dp/B07SRVV8V4"
+}
